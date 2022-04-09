8743843ccabf02e107992a4323d494736ca6ac45
Chip Stewart <donald.a.stewart@gmail.com>
2022-04-09 13:34:53 -0400

    filter segments with large gaps as well as low het density segments

--- ascat-parser_task_1/src/ascatparser.py
+++ ascat-parser_task_1/src/ascatparser.py
@@ -18,21 +18,23 @@ def parse_args(argv):
     # file with normal het sites
     output.add_argument('normal_hets', type=str, help="ASCAT normal het site output.")
     # approximate read depth
-    output.add_argument('depth', type=int, help="approximate read depth.",default=100)
+    output.add_argument('depth', type=int, help="approximate read depth.", default=100)
+    # min_het_density
+    output.add_argument('het_den_min', type=float, help="het density minimum.", default=2.5e-7)
 
-    # output acs file for ABSOLUTE
-    output.add_argument('allelic_output', type=str, help="Path for allelic capseg output.")
-    # output seg file for IGV and GISTIC
-    output.add_argument('gistic_output', type=str, help="Path for seg file output.")
-    
-    # ignore het site BAFs and use segmentedBAF
-    # **** WARNING: this option seriously undercalls het site allele shifts ****
-    output.add_argument("--use-ascat-normalization", action="store_true", help="Use segmented BAF for f calculation.")
-    
+    # output area
+    output.add_argument('output_directory', type=str, help="Path for output.", default='.')
+    # # output seg file for IGV and GISTIC
+    # output.add_argument('gistic_output', type=str, help="Path for seg file output.")
+    #
+    # # ignore het site BAFs and use segmentedBAF
+    # # **** WARNING: this option seriously undercalls het site allele shifts ****
+    # output.add_argument("--use-ascat-normalization", action="store_true", help="Use segmented BAF for f calculation.")
+    #
     return output.parse_args(argv)
     
 def helper(args):
-    return parser(args.caveman_segments, args.copynumber, args.normal_hets, args.depth, args.use_ascat_normalization)
+    return parser(args.caveman_segments, args.copynumber, args.normal_hets, args.depth, args.het_den_min)
 
 def is_between(s, lower, upper):
     return (lower <= s) & (upper >= s)
@@ -48,6 +50,7 @@ def negLL_beta2(parameters,v):
 
 def beta2fit(baf,DEP):
     NSIGMA = 2
+    tiny=1e-30
     v1 = baf
     v2 = 1-baf
     v = numpy.concatenate((v1, v2), axis=0)
@@ -58,20 +61,24 @@ def beta2fit(baf,DEP):
     if (par0[0] > 0.5):
         par0[0] = 1 - par0[0]
 
-    # Inverse Hessian -> Covariance matrix doesn't seem to work...
-    mle_model2 = minimize(negLL_beta2, par0, args=v, method='BFGS')
-    maf=mle_model2.x[0]
-    dep=mle_model2.x[1]
-    mafse = numpy.sqrt(mle_model2.hess_inv[0, 0])
-    depse = numpy.sqrt(mle_model2.hess_inv[1, 1])
+    maf = mle_model.x[0]
+    dep = mle_model.x[1]
 
+    # Inverse Hessian -> Covariance matrix doesn't seem to work...
+    # mle_model2 = minimize(negLL_beta2, par0, args=v, method='BFGS')
+    # maf=mle_model2.x[0]
+    # dep=mle_model2.x[1]
+    # mafse = numpy.sqrt(mle_model2.hess_inv[0, 0])
+    # depse = numpy.sqrt(mle_model2.hess_inv[1, 1])
     # estimate maf CI - fit maf loglike
     ll0 = negLL_beta2([maf,dep], v)
+    depse = numpy.sqrt(dep)
 
     # maf high CI 2sigma (95%) - log like up by 1 unit
-    loghigh=numpy.log10(0.5-maf)
+    loghigh=numpy.log10(0.5-maf+tiny)
     maf1 = numpy.logspace(-4 ,loghigh, num=100) + maf
-    ll1 = maf1*0
+    ll1 =  [None] * len(maf1)
+
     i = 0
     for m1 in maf1:
         ll1[i] = negLL_beta2([m1, dep], v) - ll0 - NSIGMA/2.0
@@ -83,9 +90,9 @@ def beta2fit(baf,DEP):
         mafH=float(maf1[khigh])
 
     # maf low CI
-    loglow=numpy.log10(maf)
+    loglow=numpy.log10(maf+tiny)
     maf2 = maf - numpy.logspace(-4 ,loglow, num=100)
-    ll2 = maf2*0
+    ll2 = [None] * len(maf2)
     i = 0
     for m1 in maf2:
         ll2[i] = negLL_beta2([m1, dep], v) - ll0 - NSIGMA/2.0
@@ -100,7 +107,7 @@ def beta2fit(baf,DEP):
 
     return [maf,mafCI,dep,depse]
 
-def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_ascat_normalization=False):
+def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, het_den_min ):
     # load segments from caveman
     HET_MAF_MIN   = 0.3  # het sites with MAF below not used a het
     HET_ADEP_MIN  = 7    # minimum normal allelic depth for normal het sites
@@ -108,6 +115,7 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
     TAU_SIGMA0    = 0.01 # added to sigma.tau in quadrature
     F_SIGMA0      = 0.002 # added to het MAF std err in quadrature
     HET_GAP_MAX   = 0.5 # maximum allowed gap in segment of hets (~anti centromere filter)
+    HET_DEN_MIN   = het_den_min # minimum het density (hets/bp) allowed in segment of hets (~anti low info seg)
 
     caveman_segs = pandas.read_csv(caveman_path,
                               names=['chromosome', 'start_bp', 'end_bp', 'cp_major_normal', 'cp_minor_normal',
@@ -116,11 +124,11 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
     # load tumor het site LogCR and BAF data
     cp = pandas.read_csv(copynumber_path, sep='\t')
     cp = cp[~cp['Chromosome'].astype(str).str.contains("X|Y|M")]
-    cp = cp[cp['Chromosome'].astype(int)<=22]
+    cp = cp[cp['Chromosome'].astype(int) <= 22]
     cp['site'] = cp['Chromosome'].astype(str).str.cat(cp['Position'].astype(str), sep='-')
-
+    cp['Chromosome'] = cp['Chromosome'].astype(int)
     # load matched normal het data
-    het_normal = pandas.read_csv(copynumber_normal_path, sep='\t')
+    het_normal = pandas.read_csv(copynumber_normal_path, sep='\t',dtype={'#CHR': 'str'})
     het_normal = het_normal[~het_normal['#CHR'].astype(str).str.contains("X|Y|M")]
     het_normal['site'] = het_normal['#CHR'].astype(str).str.cat(het_normal['POS'].astype(str), sep='-')
     # subset normal_hets to valid_hets (depth threshold , MAF threshold, base noise threshold)
@@ -148,6 +156,7 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
 
     longest_segment=0
     longest_segment_stdev_tau=0
+    caveman_segs['length']=caveman_segs['end_bp']-caveman_segs['start_bp']
 
     for i, row in caveman_segs.iterrows():
         # acs_df1 = pandas.DataFrame(
@@ -158,13 +167,21 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
             continue
 
         cpseg = cp[(cp['Position'] >= row['start_bp']) & (cp['Position'] <= row['end_bp']) &
-                      (cp['Chromosome'] == int(row['chromosome']))]
+                      (cp['Chromosome'].astype(int) == int(row['chromosome']))]
 
         # check for gap fraction of segment.
-        gap=numpy.amax(numpy.diff(cpseg[cpseg['het']]['Position'] ) )
-        xgap = float(gap)/(row['end_bp']-row['start_bp'])
-        if xgap>HET_GAP_MAX:
+        if sum(cpseg['het']) < 2:
             continue
+        gap=numpy.amax(numpy.diff(cpseg[cpseg['het']]['Position'] ) )
+        xgap = float(gap) /row['length']
+        if xgap > HET_GAP_MAX:
+            continue
+
+        nhet = sum(cpseg['het'])
+        hetden = float(nhet)/row['length']
+        if hetden < HET_DEN_MIN:
+            continue
+
         NTARG = cpseg['targ'].shape[0]
         if NTARG>longest_segment:
             longest_segment=NTARG
@@ -191,7 +208,7 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
                'End.bp': row['end_bp'],
                'n_probes': cpseg[cpseg['targ']].shape[0],
                'n_hets': cpseg[cpseg['het']].shape[0],
-               'f': maf1, #'f': selected['segmented BAF'].mean() if use_ascat_normalization else selected['BAF'].mean(), # mean if < 0.5 + mean 1-baf if >0.5
+               'f': maf1, #'f': selected['segmented BAF'].mean()
                'tau':tau1,
                'mu.major': mu_major1,
                'mu.minor': mu_minor1,
@@ -201,7 +218,7 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
                }
         print(i,maf1,fse,mafCI,float(numpy.diff(mafCI)),dep1,depse1)
         acs_df = acs_df.append(acs_df1.copy(), ignore_index=True, verify_integrity=True)
-        # if i>2:
+        # if i > 10:
         #     break
 
     acs_df = acs_df[acs_df['n_hets'] > 1]  # cleanup missed sites
@@ -229,7 +246,7 @@ def parser(caveman_path, copynumber_path, copynumber_normal_path, depth, use_asc
     return acs_df, seg_df, cp
 
 
-def xhg38(a,p):
+def xhg38(a, p):
     L = [248956422, 242193529, 198295559, 190214555, 181538259, 170805979,
     159345973, 145138636, 138394717, 133797422, 135086622, 133275309,
     114364328, 107043718, 101991189, 90338345, 83257441, 80373285, 58617616,
@@ -249,7 +266,7 @@ def create_plots(args, acs_df, cp):
     aL=aL[:22]
     pL=pL[:22]
 
-    plt.figure(figsize=(18, 10), dpi=100)
+    plt.figure(figsize=(18, 10), dpi=400)
     ax1 = plt.subplot(211)
     ax2 = plt.subplot(212, sharex=ax1)
 
@@ -272,9 +289,12 @@ def create_plots(args, acs_df, cp):
     x2 = xhg38(acs_df['Chromosome'].astype(int), acs_df['End.bp'])
     y1 = numpy.log2(list(acs_df['tau']/2))
     y2 = y1
+    sigma_LogR = acs_df['sigma.tau'].div(acs_df['tau']*numpy.log(2))
     #hold(True)
     for i in range(acs_df['Chromosome'].shape[0]):
-        ax1.plot([x1[i],x2[i]],[y1[i],y2[i]], ls='-', color = 'k', linewidth = 3)
+        p = plt.Rectangle((x1[i],y1[i]-sigma_LogR[i]/2), x2[i] - x1[i], sigma_LogR[i], alpha=0.91, edgecolor=None, facecolor=(0.5, 0.5, 0.5))
+        ax1.add_patch(p)
+        ax1.plot([x1[i],x2[i]],[y1[i],y2[i]], ls='-', color = 'k', linewidth = 2)
 
     ax1.grid(True)
     #hold(False)
@@ -291,7 +311,7 @@ def create_plots(args, acs_df, cp):
     ax2.set_xticks(xM)
     ax2.set_xticklabels(aL)
     for i in range(0,22,2):
-        p = plt.Rectangle((xL[i], 0), xL[i+1]-xL[i], 1, alpha=0.2, edgecolor=None, facecolor=(0.7, 0.7, 0.7))
+        p = plt.Rectangle((xL[i], 0), xL[i+1]-xL[i], 1, alpha=0.1, edgecolor=None, facecolor=(0.7, 0.7, 0.7))
         ax2.add_patch(p)
     x1 = xhg38(acs_df['Chromosome'].astype(int), acs_df['Start.bp'])
     x2 = xhg38(acs_df['Chromosome'].astype(int),acs_df['End.bp'])
@@ -299,13 +319,21 @@ def create_plots(args, acs_df, cp):
     y2 = y1
     #hold(True)
     for i in range(acs_df['Chromosome'].shape[0]):
-        ax2.plot([x1[i],x2[i]],[y1[i],y2[i]], ls='-', color = 'k', linewidth = 3)
+        ax2.plot([x1[i],x2[i]],[y1[i],y2[i]], ls='-', color = 'k', linewidth = 2)
+
+
 
     ax2.grid(True)
     #hold(False)
     #plt.show()
 
-    plt.savefig(args.caveman_segments.split('/')[-1].split('.')[0] + ".ascat.acs.png")
+    outputdir = args.output_directory
+    if outputdir[-1] != "/":
+        outputdir = outputdir + '/'
+
+    png_file=outputdir + args.caveman_segments.split('/')[-1].split('.')[0] + ".ascat.acs.png"
+
+    plt.savefig(png_file)
     
     
 if __name__ == '__main__':
@@ -316,10 +344,19 @@ if __name__ == '__main__':
     print(args)
 
     allelic_capseg, gistic, cp  = helper(args)
-    
-    allelic_capseg.to_csv(args.allelic_output, sep='\t', index=False, float_format="%.5f")
-    
-    gistic.to_csv(args.gistic_output, sep='\t', index=False, float_format="%.5f")
+
+    outputdir = args.output_directory
+    if outputdir[-1] != '/':
+        outputdir=outputdir + '/'
+
+    AllelicCapSeg_file= outputdir + args.caveman_segments.split('/')[-1].split('.')[0] + ".ascat.acs.tsv"
+
+    allelic_capseg.to_csv(AllelicCapSeg_file, sep='\t', index=False, float_format="%.5f")
+
+    Seg_file = outputdir + args.caveman_segments.split('/')[-1].split('.')[0] + ".ascat.seg"
+
+
+    gistic.to_csv(Seg_file, sep='\t', index=False, float_format="%.5f")
     
     create_plots(args, allelic_capseg, cp)
 
